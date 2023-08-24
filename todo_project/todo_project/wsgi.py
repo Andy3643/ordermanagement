c5756a418f6bd0a6b39d245195c242fc2e2aee33
@Andy <joshua.mulonga@student.moringaschool.com>
2023-08-24 04:51:59 +0300

    fix gunicorn

--- todo_project/todo_project/wsgi.py
+++ todo_project/todo_project/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.todo_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.settings')
 application = get_wsgi_application()
