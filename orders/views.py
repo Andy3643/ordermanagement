31fbe00dda1434d504b43c71374cd0136a49a03c
andy3643 <joshandy254@gmail.com>
2023-12-02 13:16:33 +0300

    ft-add pagination to the completed orders section.

--- orders/views.py
+++ orders/views.py
@@ -1,28 +1,40 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from .models import Order
 from .forms import OrderForm
-from datetime import date 
+from datetime import date
 
 def order_list(request):
-    # If form is submitted
+    # Handle form submission
     if request.method == "POST":
         form = OrderForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('order_list')
-    
-    # If viewing the page
     else:
         form = OrderForm()
-    
+
+    # Handle pending orders
     pending_orders = Order.objects.filter(is_done=False)
-    completed_orders = Order.objects.filter(is_done=True)
+
+    # Handle completed orders with pagination
+    completed_order_list = Order.objects.filter(is_done=True).order_by('-date_due')  # Ordering by date due
     
+    # Handle search
+    search_query = request.GET.get('search')
+    if search_query:
+        completed_order_list = completed_order_list.filter(order_name__icontains=search_query)
+
+    paginator = Paginator(completed_order_list, 15)  # Show 15 orders per page
+    page_number = request.GET.get('page')
+    completed_orders = paginator.get_page(page_number)
+
+    # Render the template with context
     return render(request, 'orders/order_list.html', {
         'form': form,
         'pending_orders': pending_orders,
         'completed_orders': completed_orders,
-        'today': date.today() 
+        'today': date.today()
     })
 
 def mark_order_done(request, order_id):
--- todo_project/settings.py
+++ todo_project/settings.py
@@ -24,7 +24,7 @@ load_dotenv(os.path.join(BASE_DIR, "../.env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-#SECRET_KEY = 'your_secret_key_here'
+# SECRET_KEY = 'your_secret_key_here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
