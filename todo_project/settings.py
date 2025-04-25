1603fc3c66158559fc599ab9d94ff63be918da18
Andy3643 <joshandy254@gmail.com>
2025-04-25 21:30:31 +0300

    Add CSRF_TRUSTED_ORIGINS to settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -144,6 +144,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = [
     'https://web-production-4678.up.railway.app',
-    'https://web-production-5870.up.railway.app'
+    'https://web-production-5870.up.railway.app',
+    'https://web-production-cc899.up.railway.app'
 ]
 
